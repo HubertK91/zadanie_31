54f05d585e57d59c5f7062467d6052d74586d54f
Hubert Kowalski <hubert160491@gmail.com>
2021-06-05 20:07:21 +0200

    Stworzenie projektu

---
+++ src/test/java/pl/hk/zadanie_31/Zadanie31ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hk.zadanie_31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie31ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
